36b68ba39c29c87ec01db862cefa93fc4cffc486
Renkin <906155099@qq.com>
2024-09-20 11:21:51 +0800

    deploy docs

---
+++ assets/chunks/index.CqTfHfR6.js
@@ -0,0 +1,6 @@
+import{_ as c}from"./index.CyylK2ps.js";import"./javascript.CF3M7x3g.js";import{d as m,p as n,v as p,o as i,b as u,k as d,e as f}from"./framework.LbdJDJiu.js";import"./commonjsHelpers.Cpj98o6Y.js";import"./javascript.CUAUpMbp.js";const C=m({__name:"index",setup(v){const o=n(!1),t=n(`var i = 0;
+for (; i < 9; i++) {
+    console.log(i);
+    // more statements
+}
+`),s=(a,e)=>{console.log(a,e)},r={mode:"text/javascript",theme:"default"};return p(()=>{o.value=!0}),(a,e)=>o.value?(i(),u(d(c),{key:0,value:t.value,"onUpdate:value":e[0]||(e[0]=l=>t.value=l),options:r,border:"",placeholder:"测试 placeholder",height:200,class:"cm-component",onChange:s},null,8,["value"])):f("",!0)}});export{C as default};
